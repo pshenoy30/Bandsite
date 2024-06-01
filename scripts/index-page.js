3e6eb3da715b68bbe182a3955fc234ee37f81d48
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-01 17:56:47 +0400

    Added the dynamic timestamp to the comment section

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -27,16 +27,59 @@
     });
   }
 
-  //function to format the date
-  function dateFormattor(timestamp){
-    let current = new Date(timestamp)
-    let year = current.getFullYear();
-    let month = String(current.getMonth()+1).padStart(2,"0");
-    let day = String(current.getDate()).padStart(2,"0");
+  function dynamicTimeStamp (timestamp){
+    const current = new Date();
+    const datePosted = new Date(timestamp)
+    let postedAgo = "";
+    const diff = Math.floor((current-datePosted)/1000);
 
-    return (month+"/"+day+"/"+year)
+    let yearInterval = Math.floor(diff/(60*60*24*365));
+    let monthInterval = Math.floor(diff/(60*60*24*30));
+    let weekInterval = Math.floor(diff/(60*60*24*7));
+    let dayInterval = Math.floor(diff/(60*60*24));
+    let hourInterval = Math.floor(diff/(60*60));
+    let minuteInterval = Math.floor(diff/(60));
+    let secInterval = Math.floor(diff/(1));
+
+    //if the date is years ago, then print the date
+    if (yearInterval > 1){
+      let year = datePosted.getFullYear();
+      let month = String(datePosted.getMonth()+1).padStart(2,"0");
+      let day = String(datePosted.getDate()).padStart(2,"0");
+      return (month+"/"+day+"/"+year)
+    }
+
+    if (monthInterval > 1){
+      console.log(monthInterval+" "+"months ago");
+      return monthInterval+" "+"months ago";
+    }
+
+    if (weekInterval > 1){
+      console.log(weekInterval+" "+"weeks ago");
+      return weekInterval+" "+"weeks ago";
+    }
+
+    if (dayInterval > 1){
+      console.log(dayInterval+" "+"days ago");
+      return dayInterval+" "+"days ago";
+    }
+
+    if (hourInterval > 1){
+      console.log(hourInterval+" "+"hours ago");
+      return hourInterval+" "+"hours ago";
+    }
+
+    if (minuteInterval > 1){
+      console.log(minuteInterval+" "+"mins ago");
+      return minuteInterval+" "+"mins ago";
+    }
+
+    if (secInterval >= 0){
+      console.log(secInterval+" "+"secs ago");
+      return secInterval+" "+"secs ago";
+    }
   }
-  
+
   //Function to create comment element for each person
   function createCommentElement(commentObj) {
     const commentContainerElement = createElementWithClass("article", "posted-comments__container");
@@ -53,7 +96,7 @@
   
     // Create and append time element
     const dateElement = createElementWithClass("h3","posted-comments__container__inputs__time");
-    const dateElementContent = dateFormattor(commentObj.timestamp);
+    const dateElementContent = dynamicTimeStamp(commentObj.timestamp);
     console.log(dateElementContent);
     dateElement.innerText = dateElementContent;
     commentContentElement.appendChild(dateElement);
@@ -121,8 +164,6 @@
     form.reset(); // Reset the form fields
   }
   
-  console.log(await bandApi.getComments());
-
   // Add event listener to the form for submitting a new comment
   const newCommentForm = document.querySelector(".comments__form");
   newCommentForm.addEventListener("submit", addNewComment);
